184682b2eb77c70b8494943626b2610391565267
BavbelSergey <bavbelsergey@gmail.com>
2026-05-27 23:51:35 +0000

    deploy5

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+: "${BACKEND_HOST:=backend}"
+: "${BACKEND_PORT:=8080}"
+
 envsubst '$BACKEND_HOST $BACKEND_PORT' \
     < /etc/nginx/templates/default.conf.template \
     > /etc/nginx/conf.d/default.conf
