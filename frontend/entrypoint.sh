10adf8281bbfa3519a7056589b244a0791167a00
BavbelSergey <bavbelsergey@gmail.com>
2026-05-28 00:04:16 +0000

    123

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-: "${BACKEND_HOST:=backend}"
-: "${BACKEND_PORT:=8080}"
+echo "=== ENTRYPOINT START ==="
+echo "BACKEND_HOST=$BACKEND_HOST"
+echo "BACKEND_PORT=$BACKEND_PORT"
 
 envsubst '$BACKEND_HOST $BACKEND_PORT' \
     < /etc/nginx/templates/default.conf.template \
     > /etc/nginx/conf.d/default.conf
 
-echo "Nginx configured with BACKEND_HOST=$BACKEND_HOST and BACKEND_PORT=$BACKEND_PORT"
+echo "=== GENERATED NGINX CONFIG ==="
+cat /etc/nginx/conf.d/default.conf
+echo "=== END CONFIG ==="
 
 nginx -g 'daemon off;'
